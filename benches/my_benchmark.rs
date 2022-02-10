87680f7ca0b25e19a72fd7920d182f090ace5637
europeanplaice <paul.thomas.goodfellas@gmail.com>
2022-02-10 23:24:50 +0900

    Change benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -3,13 +3,16 @@ use subset_sum::dp::find_subset;
 use subset_sum::dp::find_subset_fast_only_positive;
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let mut a = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    a.sort();
+    let a2: Vec<u32> = a.iter().map(|x| *x as u32).collect();
     c.bench_function("find_subset", 
-        |b| b.iter(|| find_subset(black_box(&vec![1, 2, 3, 4, 5]), black_box(10))));
+        |b| b.iter(|| find_subset(black_box(&a), black_box(21))));
 
     c.bench_function("find_subset_fast_only_positive", 
     |b| b.iter(|| 
-        find_subset_fast_only_positive(black_box(&vec![1, 2, 3, 4, 5]), 
-        black_box(10))));
+        find_subset_fast_only_positive(black_box(&a2), 
+        black_box(21))));
 }
 
 criterion_group!(benches, criterion_benchmark);
