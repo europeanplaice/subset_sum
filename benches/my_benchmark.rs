c97c425bc0b2b1c620086cac4c213e1993f0ad05
europeanplaice <paul.thomas.goodfellas@gmail.com>
2022-02-12 20:11:23 +0900

    skip method on some conditions

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -3,16 +3,20 @@ use subset_sum::dp::find_subset;
 use subset_sum::dp::find_subset_fast_only_positive;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut a = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-    a.sort();
+    let a = vec![5, 6, 7, 8, 9, 10, 11];
     let a2: Vec<u32> = a.iter().map(|x| *x as u32).collect();
     c.bench_function("find_subset", 
-        |b| b.iter(|| find_subset(black_box(&a), black_box(21))));
+        |b| b.iter(|| find_subset(&a, 21)));
 
     c.bench_function("find_subset_fast_only_positive", 
     |b| b.iter(|| 
-        find_subset_fast_only_positive(black_box(&a2), 
-        black_box(21))));
+        find_subset_fast_only_positive(&a2, 
+        21)));
+
+    let d = vec![-3,  10,  56, -33,  65,  -9,   8,  72,  63,  35];
+    c.bench_function("find_subset_complicated", 
+        |b| b.iter(|| find_subset(&d, 7)));
+
 }
 
 criterion_group!(benches, criterion_benchmark);
